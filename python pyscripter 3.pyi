ff40ce16c3de9ce47ecca182aac4da25c8c72b98
amitkhambra <139910660+amitkhambra@users.noreply.github.com>
2023-07-25 17:05:39 +0530

    Add files via upload

---
+++ python pyscripter 3.pyi
@@ -0,0 +1,76 @@
+#name=input(" what is your name?")
+#print("hello"+ name)
+
+
+#old_age = int(input(" enter your age"))
+#new_age = old_age +2
+#print(new_age)
+
+
+# comvert into float number
+
+'''mumber= 19
+print(float(19))
+
+first= int(input(" enter a  first number"))
+second= int(input("enter  second number"))
+sum = (first+ second)
+print(sum)
+
+first= int(input(" enter a  first number"))
+second=int(input("enter  second number"))
+sum = (first)+ (second)
+print(" the sum is : "+ str(sum))
+
+
+
+
+
+name="tony stark"
+print(name.upper())
+print(name.lower())
+print(name.find('stark'))
+print(name.replace('tony stark', 'iron man'))
+
+
+print('t'in name)
+
+
+
+
+i=5
+i+=2
+print(i)
+
+# comparison operator
+
+print(3 != 2)'''
+
+# calculator
+
+
+
+first= int(input(" enter first number "))
+operator= input("enter operator (+,-,*,/,%)")
+second=int(input(" enter second number "))
+if operator=="+":
+    print(" the sum is",first+ second)
+elif operator=="-":
+    print(" the sum is ",first-second)
+elif operator=="*":
+    print("the sum is ",first* second)
+elif operator=="/":
+    print("the sum is ",first/second)
+elif operator=="%":
+    print("the sum is " ,first% second)
+
+else :
+    print("invalid operator")
+
+
+
+
+
+
+
+
